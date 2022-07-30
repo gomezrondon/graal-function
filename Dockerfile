debcc24d9fce5848013b1836642dfce43192ac67
gomezrondon <gomezrondon@gmail.com>
2022-07-30 08:38:38 -0600

    updating spring aot 0.12.1

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM adoptopenjdk/openjdk11:alpine-slim as builder
+ADD /build/libs/graal-function-0.0.1-SNAPSHOT.jar application.jar
+
+RUN java -Djarmode=layertools -jar application.jar extract
+
+FROM adoptopenjdk:11-jre-hotspot
+COPY --from=builder dependencies/ ./
+COPY --from=builder snapshot-dependencies/ ./
+COPY --from=builder spring-boot-loader/ ./
+COPY --from=builder application/ ./
+ENTRYPOINT ["java", "org.springframework.boot.loader.JarLauncher"]
--- src/main/java/com/example/graalfunction/GraalFunctionApplication.java
+++ src/main/java/com/example/graalfunction/GraalFunctionApplication.java
@@ -4,7 +4,36 @@ import com.gomezrondon.cloudruntest.TDCProcess;
 import com.gomezrondon.cloudruntest.entities.Param;
 import com.gomezrondon.cloudruntest.entities.Payload;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.batch.BatchAutoConfiguration;
+import org.springframework.boot.autoconfigure.cache.CacheAutoConfiguration;
+import org.springframework.boot.autoconfigure.cassandra.CassandraAutoConfiguration;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.cassandra.CassandraDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.cassandra.CassandraReactiveDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.couchbase.CouchbaseDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.couchbase.CouchbaseReactiveDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.elasticsearch.ElasticsearchDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebAutoConfiguration;
+import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration;
+import org.springframework.boot.autoconfigure.graphql.GraphQlAutoConfiguration;
+import org.springframework.boot.autoconfigure.http.codec.CodecsAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.XADataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.activemq.ActiveMQAutoConfiguration;
+import org.springframework.boot.autoconfigure.jms.artemis.ArtemisAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.embedded.EmbeddedMongoAutoConfiguration;
+import org.springframework.boot.autoconfigure.sendgrid.SendGridAutoConfiguration;
+import org.springframework.boot.autoconfigure.solr.SolrAutoConfiguration;
+import org.springframework.boot.autoconfigure.transaction.TransactionAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.reactive.WebFluxAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.reactive.WebSessionIdResolverAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.reactive.error.ErrorWebFluxAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.reactive.function.client.WebClientAutoConfiguration;
+import org.springframework.boot.autoconfigure.webservices.WebServicesAutoConfiguration;
+import org.springframework.boot.autoconfigure.webservices.client.WebServiceTemplateAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.reactive.WebSocketReactiveAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -13,6 +42,40 @@ import java.util.function.Supplier;
 
 
 @SpringBootApplication
+@EnableAutoConfiguration(exclude = {
+		XADataSourceAutoConfiguration.class
+		, WebSocketReactiveAutoConfiguration.class
+, WebSessionIdResolverAutoConfiguration.class
+, WebServicesAutoConfiguration.class
+, WebServiceTemplateAutoConfiguration.class
+, WebFluxAutoConfiguration.class,
+		WebClientAutoConfiguration.class
+, TransactionAutoConfiguration.class
+, SolrAutoConfiguration.class
+, SpringDataWebAutoConfiguration.class
+, SendGridAutoConfiguration.class
+, ActiveMQAutoConfiguration.class
+		, ArtemisAutoConfiguration.class
+		, BatchAutoConfiguration.class
+, CacheAutoConfiguration.class
+, CassandraAutoConfiguration.class
+, CassandraDataAutoConfiguration.class
+		, CassandraReactiveDataAutoConfiguration.class
+, CodecsAutoConfiguration.class
+		, CouchbaseAutoConfiguration.class
+, CouchbaseDataAutoConfiguration.class
+		, CouchbaseReactiveDataAutoConfiguration.class
+, DataSourceAutoConfiguration.class
+		, ElasticsearchDataAutoConfiguration.class
+, EmbeddedMongoAutoConfiguration.class
+, ErrorWebFluxAutoConfiguration.class
+		, FlywayAutoConfiguration.class
+		, GraphQlAutoConfiguration.class
+
+
+
+
+})
 public class GraalFunctionApplication {
 
 	public static void main(String[] args) {
